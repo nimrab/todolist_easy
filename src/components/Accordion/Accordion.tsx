107ca29bbb9a914adf7e7c5e5455c6916ac6a90e
nimrab <nimrab.0@gmail.com>
2021-10-19 21:19:55 +0300

    List+rating(true/false cond)

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+
+type AccordionPropsType = {
+    title: string,
+    collapsed: boolean
+}
+
+function Accordion(props: AccordionPropsType) {
+
+    return (
+        <>
+            <AccordionTitle title={props.title}/>
+            {!props.collapsed && <AccordionBody/>}
+            {/*если collapsed false, то смотрм <Ac.Bo.>. Он True. Значение не переключаем, возвращаем false. {false} не отрисуется*/}
+            {/*если collapsed true, то смотрм <Ac.Bo.>. Он True. Вернем значение, на котором остановились, те <Ac.Bo.>. Он отобразится */}
+
+        </>
+    )
+}
+
+// решение ниже можно оптимизировать с помощью логического условия
+//     if (props.collapsed) {
+//             return (
+//         AccordionTitle title={props.title}/>
+//         )
+//
+//
+//
+//     }
+// // } else {
+// //     return (
+// //         <>
+// //             <AccordionTitle title={props.title}/>
+// //             <AccordionBody/>
+// //         </>
+// //     )
+// // }
+
+
+type AccordionTitlePropsType = {
+    title: string
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+    return (
+        <h3>{props.title}</h3>
+    )
+}
+
+
+function AccordionBody() {
+    return (
+        <ul>
+            <li className={'list1'}>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
+
+export default Accordion;
---
+++ src/components/Rating/Rating.tsx
@@ -0,0 +1,108 @@
+import React from "react";
+
+
+type RatingPropsType = {
+    value: 1 | 2 | 3 | 4 | 5
+}
+
+
+export function Rating(props: RatingPropsType) {
+    return (
+        <div>
+            <Star value={props.value > 0}/>
+            <Star value={props.value > 1}/>
+            <Star value={props.value > 2}/>
+            <Star value={props.value > 3}/>
+            <Star value={props.value > 4}/>
+        </div>
+    )
+}
+
+
+type StarPropsType = {
+    value: boolean
+}
+
+function Star(props: StarPropsType) {
+
+    if (props.value) {
+        return (
+            <>
+                <span> <b>STAR</b> </span>
+                {/*props.value && <span> <b>STAR</b> </span>*/} {/*хорошо рисует жирную звезду, но не ресуютпустую при false*/}
+            </>
+        )
+    } else {
+        return (
+            <>
+                <span> STAR </span>
+            </>
+        )
+
+    }
+
+    //выше рефакторим код
+    // switch (props.value) {
+    //   case 1:
+    //     return (
+    //         <>
+    //           <span><b>STAR </b></span>
+    //           <span>STAR </span>
+    //           <span>STAR </span>
+    //           <span>STAR </span>
+    //           <span>STAR </span>
+    //         </>
+    //     )
+    //   case 2:
+    //     return (
+    //         <>
+    //           <span><b>STAR </b></span>
+    //           <span><b>STAR </b></span>
+    //           <span>STAR </span>
+    //           <span>STAR </span>
+    //           <span>STAR </span>
+    //         </>
+    //     )
+    //   case 3:
+    //     return (
+    //         <>
+    //           <span><b>STAR </b></span>
+    //           <span><b>STAR </b></span>
+    //           <span><b>STAR </b></span>
+    //           <span>STAR </span>
+    //           <span>STAR </span>
+    //         </>
+    //     )
+    //   case 4:
+    //     return (
+    //         <>
+    //           <span><b>STAR </b></span>
+    //           <span><b>STAR </b></span>
+    //           <span><b>STAR </b></span>
+    //           <span><b>STAR </b></span>
+    //           <span>STAR </span>
+    //         </>
+    //     )
+    //   case 5:
+    //     return (
+    //         <>
+    //           <span><b>STAR </b></span>
+    //           <span><b>STAR </b></span>
+    //           <span><b>STAR </b></span>
+    //           <span><b>STAR </b></span>
+    //           <span><b>STAR </b></span>
+    //         </>
+    //     )
+    //   default:
+    //     return (
+    //         <>
+    //           <span>STAR </span>
+    //           <span>STAR </span>
+    //           <span>STAR </span>
+    //           <span>STAR </span>
+    //           <span>STAR </span>
+    //         </>
+    //     )
+    // }
+
+}
